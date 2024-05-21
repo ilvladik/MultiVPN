95b3b0025ebff73a768c5bcc24023f918df7dc1b
ilvladik <vlad.ilyin.2003@gmail.com>
2024-05-21 12:12:37 +0300

    Add Helpers for working with URI, Add Result Errors and Localizations

---
+++ src/OutlineServerApi/Application/Errors/ErrorDescriber.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Extensions.Localization;
+using SharedKernel.Results;
+
+namespace OutlineServerApi.Application.Errors
+{
+    internal class ErrorDescriber
+    {
+        private readonly IStringLocalizer<ErrorDescriber> _stringLocalizer;
+
+        public ErrorDescriber(IStringLocalizer<ErrorDescriber> stringLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+        }
+
+        public Error CountryWithThisNameAlreadyExists(string name)
+        {
+            return new Error()
+            {
+                Code = nameof(CountryWithThisNameAlreadyExists),
+                Description = _stringLocalizer[nameof(CountryWithThisNameAlreadyExists), name]
+            };
+        }
+
+        public Error CountryNotFound()
+        {
+            return new Error()
+            {
+                Code = nameof(CountryNotFound),
+                Description = _stringLocalizer[nameof(CountryNotFound)]
+            };
+        }
+
+        public Error KeyNotFound()
+        {
+            return new Error()
+            {
+                Code = nameof(KeyNotFound),
+                Description = _stringLocalizer[nameof(KeyNotFound)]
+            };
+        }
+
+        public Error OnlyOneKeyAllowed()
+        {
+            return new Error()
+            {
+                Code = nameof(OnlyOneKeyAllowed),
+                Description = _stringLocalizer[nameof(OnlyOneKeyAllowed)]
+            };
+        }
+
+
+        public Error ServerNotFound()
+        {
+            return new Error()
+            {
+                Code = nameof(ServerNotFound),
+                Description = _stringLocalizer[nameof(ServerNotFound)]
+            };
+        }
+
+        public Error ServerNotAvailable(string ip)
+        {
+            return new Error()
+            {
+                Code = nameof(ServerNotAvailable),
+                Description = _stringLocalizer[nameof(ServerNotAvailable), ip]
+            };
+        }
+
+        public Error AllServersNotAvailable()
+        {
+            return new Error()
+            {
+                Code = nameof(AllServersNotAvailable),
+                Description = _stringLocalizer[nameof(AllServersNotAvailable)]
+            };
+        }
+
+        public Error ServersHaveNotSameCountries(string ipOne, string ipTwo)
+        {
+            return new Error()
+            {
+                Code = nameof(ServersHaveNotSameCountries),
+                Description = _stringLocalizer[nameof(ServersHaveNotSameCountries), ipOne, ipTwo]
+            };
+        }
+
+        public Error InvalidServerAddress(string serverAddress)
+        {
+            return new Error()
+            {
+                Code = nameof(InvalidServerAddress),
+                Description = _stringLocalizer[nameof(InvalidServerAddress), serverAddress]
+            };
+        }
+
+        public Error ServerWithThisAddressAlreadyExists(string serverAddress)
+        {
+            return new Error()
+            {
+                Code = nameof(ServerWithThisAddressAlreadyExists),
+                Description = _stringLocalizer[nameof(ServerWithThisAddressAlreadyExists), serverAddress]
+            };
+        }
+        public Error KeyNotBelongToYou()
+        {
+            return new Error()
+            {
+                Code = nameof(KeyNotBelongToYou),
+                Description = _stringLocalizer[nameof(KeyNotBelongToYou)]
+            };
+        }
+    }
+}
---
+++ src/OutlineServerApi/Helpers/OutlineApiHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace OutlineServerApi.Helpers
+{
+    internal static class OutlineApiHelper
+    {
+        public static string GetHostname(string apiUrl) =>
+            new UriBuilder(apiUrl).Host;
+
+        public static int GetPort(string apiUrl) =>
+            new UriBuilder(apiUrl).Port;
+
+        public static string GetApiPrefix(string apiUrl) =>
+            new UriBuilder(apiUrl).Path.Trim('/');
+
+        public static Uri GetUri(string apiUrl) =>
+            new UriBuilder(apiUrl).Uri;
+
+        public static Uri GetUri(string hostname, int port, string apiPrefix) =>
+            new UriBuilder(Uri.UriSchemeHttps, hostname, port, apiPrefix).Uri;
+
+    }
+}
