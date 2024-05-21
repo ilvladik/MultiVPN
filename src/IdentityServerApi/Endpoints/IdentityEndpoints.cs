84e7b6d50941cbfd5af069bab68c4419b6f02dfb
ilvladik <vlad.ilyin.2003@gmail.com>
2024-05-21 11:58:27 +0300

    Add Endpoints for Identity functions and Extensions of Result Api

---
+++ src/IdentityServerApi/Endpoints/IdentityEndpoints.cs
@@ -0,0 +1,78 @@
+﻿using IdentityServerApi.Application.Dtos.Requests;
+using IdentityServerApi.Application.Dtos.Responses;
+using IdentityServerApi.Application.Features.Commands;
+using IdentityServerApi.Application.Features.Queries;
+using IdentityServerApi.Endpoints.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Results;
+
+namespace IdentityServerApi.Endpoints
+{
+    public static class IdentityEndpoints
+    {
+        public static void MapIdentityEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("");
+
+            group.MapPost("/register", async ([FromBody] RegisterRequest request, [FromQuery] string? callbackUri, ISender sender) =>
+            {
+                Result result = await sender.Send(new RegisterCommand(request.Email, request.Password, callbackUri));
+                return result.ToApiResult();
+            });
+
+            group.MapPost("/login",
+                async ([FromBody] LoginRequest request, ISender sender) =>
+            {
+                Result<AccessTokenResponse> result = await sender.Send(new LoginCommand(request.Email, request.Password));
+                return result.ToApiResult();
+            });
+
+            group.MapPost("/refresh", 
+                async ([FromBody] RefreshRequest request, ISender sender) =>
+            {
+                Result<AccessTokenResponse> result = await sender.Send(new RefreshCommand(request.RefreshToken));
+                return result.ToApiResult();
+            });
+
+            group.MapGet("/confirmEmail",
+                async ([FromQuery] string userId, [FromQuery] string code, [FromQuery] string? callbackUri, ISender sender) =>
+            {
+                Result result = await sender.Send(new ConfirmEmailCommand(userId, code));
+                if (callbackUri is not null)
+                    return Results.Redirect(callbackUri);
+                return result.ToApiResult();
+            })
+            .WithMetadata(new EndpointNameMetadata("confirmEmail")); ;
+
+            group.MapPost("/resendConfirmationEmail", 
+                async ([FromBody] ResendConfirmationEmailRequest request, ISender sender) =>
+            {
+                Result result = await sender.Send(new ResendConfirmationEmailCommand(request.Email));
+                return result.ToApiResult();
+            });
+
+            group.MapPost("/forgotPassword", 
+                async ([FromBody] ForgotPasswordRequest request, ISender sender) =>
+            { 
+                Result result = await sender.Send(new ForgotPasswordCommand(request.Email));
+                return result.ToApiResult();
+            });
+
+            group.MapPost("/resetPassword", 
+                async ([FromBody] ResetPasswordRequest request, ISender sender) =>
+            {
+                Result result = await sender.Send(new ResetPasswordCommand(request.Email, request.ResetCode, request.NewPassword));
+                return result.ToApiResult();
+            });
+
+            group.MapGet("/account",
+            async (ISender sender) =>
+            {
+                Result result = await sender.Send(new GetUserQuery());
+                return result.ToApiResult();
+            })
+                .RequireAuthorization("AuthenticatedUser");
+        }
+    }
+}
---
+++ src/IdentityServerApi/Program.cs
@@ -0,0 +1,116 @@
+﻿using IdentityServerApi.Application.Errors;
+using IdentityServerApi.Core;
+using IdentityServerApi.Core.Models;
+using IdentityServerApi.Endpoints;
+using IdentityServerApi.Endpoints.Handlers;
+using IdentityServerApi.Infrastructure.Email;
+using IdentityServerApi.Infrastructure.Jwt;
+using IdentityServerApi.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("UserPolicy", policy =>
+            policy
+                .RequireRole("User")
+                .RequireClaim(ClaimTypes.NameIdentifier))
+    .AddPolicy("AdminPolicy", policy =>
+            policy
+                .RequireRole("Admin")
+                .RequireClaim(ClaimTypes.NameIdentifier))
+    .AddPolicy("AuthenticatedUser", policy =>
+            policy
+                .RequireAuthenticatedUser()
+                .RequireClaim(ClaimTypes.NameIdentifier));
+
+builder.Services.AddCors();
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddDbContext<IdentityContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddMediatR(c =>
+    c.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+builder.Services
+    .AddIdentityCore<User>(options =>
+    {
+        options.SignIn.RequireConfirmedEmail = true;
+        options.User.RequireUniqueEmail = true;
+        
+    })
+    .AddRoles<IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddRoleManager<RoleManager<IdentityRole<Guid>>>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddErrorDescriber<LocalizedIdentityErrorDescriber>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetRequiredSection(JwtOptions.Name));
+builder.Services.Configure<EmailOptions>(builder.Configuration.GetRequiredSection(EmailOptions.Name));
+builder.Services.Configure<ExternalLinksOptions>(builder.Configuration.GetRequiredSection(ExternalLinksOptions.Name));
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IJwtProvider, JwtProvider>();
+builder.Services.AddScoped<IEmailLinkProvider, EmailLinkProvider>();
+builder.Services.AddScoped<IEmailSender<User>, EmailSender>();
+builder.Services.AddScoped<ErrorDescriber>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+builder.Services.AddLocalization(o => o.ResourcesPath = "Resources");
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+app.UseExceptionHandler(o => { });
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+CultureInfo[] supportedCultures =
+[
+    new CultureInfo("en"),
+    new CultureInfo("ru")
+];
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("ru"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures,
+    ApplyCurrentCultureToResponseHeaders = true
+});
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapIdentityEndpoints();
+
+app.Run();
